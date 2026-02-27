5d333a94de42e8c49fb39831b332be687040a676
Luisa <luisamosquera1112@gmail.com>
2026-02-27 01:07:29 -0500

    Se añade: Cada ítem tiene un botón '✕' que al hacer clic elimina ese ítem del DOM.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const listTasks = document.getElementById("task-list");
 const taskButton = document.getElementById("add-task-btn");
+let deleteButton;
+
+let listCounter = 1;
 function isListEmpty() {
   if (listTasks.childElementCount == 0) {
-    listTasks.insertAdjacentHTML(
-      "beforeend",
-      "<p id='empty-message'>No tasks yet</p>",
-    );
+    listTasks.innerHTML = "<p id='empty-message'>No tasks yet</p>";
     return true;
   }
   return false;
@@ -19,7 +19,17 @@ function addTask() {
   let inputTask = document.getElementById("task-input");
   const emptyMessage = document.getElementById("empty-message");
   if (inputTask.value.trim() !== "") {
-    listTasks.insertAdjacentHTML("beforeend", "<li>" + inputTask.value + "</li>");
+    listTasks.insertAdjacentHTML(
+      "beforeend",
+      "<li id='task-item-" +
+        listCounter +
+        "' class='task-item'>" +
+        inputTask.value +
+        " <button id='delete-task-btn-" +
+        listCounter +
+        "' class='delete-btn'>x</button></li>",
+    );
+    listCounter++;
     inputTask.value = "";
     if (emptyMessage) {
       emptyMessage.remove();
@@ -29,4 +39,17 @@ function addTask() {
 
 taskButton.addEventListener("click", (event) => {
   addTask();
+});
+
+function deleteTask(id) {
+  let taskItem = document.getElementById(id);
+  taskItem.remove();
+  isListEmpty();
+}
+
+document.addEventListener("click", (event) => {
+  const button = event.target.closest(".delete-btn");
+  if (!button) return;
+  const taskList = button.closest(".task-item");
+  deleteTask(taskList.id);
 });
