799ba4d71dbfb85bc28a7b1428c105528f9caac1
Luisa <luisamosquera1112@gmail.com>
2026-02-27 02:30:51 -0500

    Se añaden cambios en funciones para optimizar, se añade favicon.ico

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const listTasks = document.getElementById("task-list");
 const taskButton = document.getElementById("add-task-btn");
- let taskCounter = document.getElementById("task-counter");
+let taskCounter = document.getElementById("task-counter");
 let listCounter = 1;
 let listCompletedCounter = 0;
 function isListEmpty() {
   if (listTasks.childElementCount == 0) {
     listTasks.innerHTML = "<p id='empty-message'>No tasks yet</p>";
-    return true;
   }
-  return false;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -35,7 +33,6 @@ function addTask() {
     }
     updateCounter();
     listCounter++;
-    
   }
 }
 
@@ -47,35 +44,36 @@ function deleteTask(id) {
   let taskItem = document.getElementById(id);
   taskItem.remove();
   isListEmpty();
+  updateCounter();
 }
 
 document.addEventListener("click", (event) => {
   const button = event.target.closest(".delete-btn");
-  if (!button) return;
-  const taskList = button.closest(".task-item");
-  deleteTask(taskList.id);
+  if (button) {
+    const taskList = button.closest(".task-item");
+    deleteTask(taskList.id);
+    return;
+  }
+  const task = event.target.closest(".task-item");
+  if (task) {
+    markAsCompleted(task.id);
+    return;
+  }
 });
 
 function markAsCompleted(id) {
   let taskItem = document.getElementById(id);
-  if (taskItem.style.textDecoration === "line-through") {
-    taskItem.style.textDecoration = "none";
-    taskItem.style.color = "black";
-    listCompletedCounter--;
-    updateCounter();
-    
-  } else {
-    taskItem.style.textDecoration = "line-through";
-    taskItem.style.color = "gray";
+
+  taskItem.classList.toggle("completed");
+
+  if (taskItem.classList.contains("completed")) {
     listCompletedCounter++;
-    updateCounter();
+  } else {
+    listCompletedCounter--;
   }
+
+  updateCounter();
 }
-document.addEventListener("click", (event) => {
-  const task = event.target.closest(".task-item");
-  if (!task) return;
-  markAsCompleted(task.id);
-});
 
 function updateCounter() {
   const total = listTasks.querySelectorAll(".task-item").length;
