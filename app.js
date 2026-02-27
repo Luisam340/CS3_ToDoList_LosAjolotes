83d425f4c2add5b12c280761d3125e353e5c3a17
Luisa <luisamosquera1112@gmail.com>
2026-02-27 00:00:35 -0500

    Se añade opción de agregar tareas

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+const listTasks = document.getElementById("task-list");
+const taskButton = document.getElementById("add-task-btn");
 function isListEmpty() {
-    const listTasks = document.getElementById("task-list");
-    if (listTasks.childElementCount == 0) {
-        listTasks.insertAdjacentHTML("beforeend", "<p>No tasks yet</p>");
-        return true;
-    }
-    return false;
+  if (listTasks.childElementCount == 0) {
+    listTasks.insertAdjacentHTML(
+      "beforeend",
+      "<p id='empty-message'>No tasks yet</p>",
+    );
+    return true;
+  }
+  return false;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    isListEmpty();
+  isListEmpty();
+});
+
+function addTask() {
+  let inputTask = document.getElementById("task-input").value;
+  const emptyMessage = document.getElementById("empty-message");
+  if (inputTask.trim() !== "") {
+    listTasks.insertAdjacentHTML("beforeend", "<li>" + inputTask + "</li>");
+    if (emptyMessage) {
+      emptyMessage.remove();
+    }
+  }
+}
+
+taskButton.addEventListener("click", (event) => {
+  addTask();
 });
